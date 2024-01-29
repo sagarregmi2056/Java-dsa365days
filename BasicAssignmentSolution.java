8f41255b96b25c8c6379308a93187a3e98d5c4f5
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-01-29 18:23:50 +0545

    first commit

---
+++ BasicAssignmentSolution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BasicAssignmentSolution {
+
+    public static void main(String[] args) {
+
+        Scanner Scannerobj = new Scanner(System.in);
+
+        System.out.println("enter a first number");
+
+        int firstnum = Scannerobj.nextInt();
+
+        System.out.println("enter a second number");
+        int secondnum = Scannerobj.nextInt();
+
+        System.out.println("enter a third  number ");
+
+        int thirdnum = Scannerobj.nextInt();
+
+        int sum = 0;
+
+        sum = (firstnum + secondnum + thirdnum) / 3;
+
+        System.out.println("the sum of those number is" + sum);
+    }
+
+}
---
+++ Basics.java
@@ -0,0 +1,10 @@
+
+public class Basics {
+
+    // main function
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+
+    }
+
+}
