c754c01975aabf910812e735be25237a87807dea
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-01-30 16:15:18 +0545

    pattern programming dynamic

--- PatternProgramming.java
+++ PatternProgramming.java
@@ -12,15 +12,41 @@
 
 public class PatternProgramming {
 
+    // public static void main(String[] args) {
+
+    // int N = 4;
+
+    // for (int i = 0; i < N; i++) {
+
+    // for (int j = 0; j < N; j++) {
+
+    // if (i == 0 || i == 3 || j == 0 || j == 3) {
+
+    // System.out.print("*");
+
+    // }
+
+    // else
+    // System.out.print(" ");
+
+    // }
+
+    // System.out.println();
+
+    // }
+
+    // }
+
+    // for the above code we can make dynamically instead of hardcoded
     public static void main(String[] args) {
 
-        int N = 4;
+        int N = 50;
 
         for (int i = 0; i < N; i++) {
 
             for (int j = 0; j < N; j++) {
 
-                if (i == 0 || i == 3 || j == 0 || j == 3) {
+                if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
 
                     System.out.print("*");
 
